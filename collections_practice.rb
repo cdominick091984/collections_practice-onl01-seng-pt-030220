de87e631a604a2a7d0a5a17e6689469e6d22cb0e
Domenico Cascone <dcascone84@gmail.com>
2020-03-12 20:24:01 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -37,7 +37,5 @@ def sum_array(array_of_i)
 end
 
 def add_s(arr_of_str)
-  arr_of_str.each_with_index.collect { |ele, index| ele.insert(-1, "s") unless ele[index] == arr_of_str[1]
-
-  end}
+  arr_of_str.each_with_index.collect { |ele, index| ele.insert(-1, "s") }
 end
