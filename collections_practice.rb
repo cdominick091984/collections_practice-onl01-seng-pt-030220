a3aa16a130ecccb20dfdfe642260261fcd4bf130
Domenico Cascone <dcascone84@gmail.com>
2020-03-12 20:53:18 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -39,6 +39,6 @@ end
 def add_s(arr_of_str)
   arr_of_str.each_with_index.collect do |ele, index|
      ele.insert(-1, "s")
-      unless ele == arr_of_str[1]}
+      unless ele == arr_of_str[1]
       end
 end
